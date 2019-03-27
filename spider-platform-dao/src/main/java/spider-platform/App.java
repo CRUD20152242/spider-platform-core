cb971404988927631e21699b9c43f7737147cfbd
CRUD20152242 <2314449060@qq.com>
2019-03-27 23:54:52 +0800

    初始化提交

---
+++ spider-platform-dao/src/main/java/spider-platform/App.java
@@ -0,0 +1,13 @@
+package spider-platform;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
